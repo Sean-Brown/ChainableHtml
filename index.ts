70d25cfbadfd61787840b1ad813482eda0148721
Sean Brown <smbrown3706@gmail.com>
2016-05-04 15:33:09 -0400

    added elements, update structure

--- index.ts
+++ index.ts
@@ -9,22 +9,39 @@ function htmlEsc(val:string): string {
         .replace(/"/g, "&quot;")
         .replace(/'/g, "&#039;");
 }
-export class BaseChainableOptions {
+export class BaseOptions {
     public constructor(public _id:string = "", public _class:string = "", public _style:string = "") {
         this._id = esc(this._id);
         this._class = esc(this._class);
         this._style = esc(this._style);
     }
 }
-export class ClickableOptions extends BaseChainableOptions {
+export enum RefTargetType {
+    blank, parent, self, top
+}
+export class RefOptions extends BaseOptions {
+    constructor(public _href:string = "", public _target:string = "_blank", _id:string = "", _class:string = "", _style:string = "") {
+        super(_id, _class, _style);
+    }
+    static target(type:RefTargetType): string {
+        switch (type) {
+            default:
+            case RefTargetType.blank: return "_blank";
+            case RefTargetType.parent: return "_parent";
+            case RefTargetType.self: return "_self";
+            case RefTargetType.top: return "_top";
+        }
+    }
+}
+export class ImgOptions extends BaseOptions {
+    constructor(public _src:string = "", public _alt:string = "", _id:string = "", _class:string = "", _style:string = "") {
+        super(_id, _class, _style);
+    }
+}
+export class ClickableOptions extends BaseOptions {
     public constructor(_id:string = "", _class:string = "", _style:string = "", public _onclick:string = "") {
         super(_id, _class, _style);
         this._onclick = esc(this._onclick);
-    }
-}
-export class ListItemOptions extends ClickableOptions {
-    public constructor(_id:string = "", _class:string = "", _style:string = "", _onclick:string = "", public value = "") {
-        super(_id, _class, _style);
     }
 }
 export class ChainableTags {
@@ -38,58 +55,98 @@ export class ChainableTags {
     static get ol():string { return "ol"; }
     static get li():string { return "ol"; }
     static get span():string { return "span"; }
+    static get a():string { return "a"; }
+    static get button():string { return "button"; }
+    static get img():string { return "img"; }
 }
 export class ChainableHtml {
     public constructor(public html:string = "") {
     }
-    protected baseEl(tag:string, opts: BaseChainableOptions): ChainableHtml {
+    protected baseEl(tag:string, opts:BaseOptions = null): ChainableHtml {
+        if (opts == null)
+            opts = new BaseOptions();
         this.html += `<${tag} id='${opts._id}' class='${opts._class}' style='${opts._style}'>`;
+        return this;
+    }
+    protected baseRefEl(tag:string, opts:RefOptions = null): ChainableHtml {
+        if (opts == null)
+            opts = new RefOptions();
+        this.html += `<${tag} id='${opts._id}' class='${opts._class}' style='${opts._style}' href='${opts._href}' target='${opts._target}'>`;
+        return this;
+    }
+    protected baseImgEl(tag:string, opts:ImgOptions = null): ChainableHtml {
+        if (opts == null)
+            opts = new ImgOptions();
+        this.html += `<${tag} id='${opts._id}' class='${opts._class}' style='${opts._style}' src='${opts._src}' alt='${opts._alt}'>`;
         return this;
     }
     protected closeEl(tag:string): ChainableHtml {
         this.html += `</${tag}>`;
         return this;
     }
-    div(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.div, opts); }
+    val(str:string): ChainableHtml
+    {
+        this.html += htmlEsc(str);
+        return this;
+    }
+    br(): ChainableHtml
+    {
+        this.html += "<br />";
+        return this;
+    }
+
+    div(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.div, opts); }
     endDiv(): ChainableHtml { return this.closeEl(ChainableTags.div); }
 
-    p(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.p, opts); }
+    p(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.p, opts); }
     endP(): ChainableHtml { return this.closeEl(ChainableTags.p); }
 
-    h1(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.h1, opts); }
+    h1(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.h1, opts); }
     endH1(): ChainableHtml { return this.closeEl(ChainableTags.h1); }
-    h2(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.h2, opts); }
+    h2(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.h2, opts); }
     endH2(): ChainableHtml { return this.closeEl(ChainableTags.h2); }
-    h3(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.h3, opts); }
+    h3(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.h3, opts); }
     endH3(): ChainableHtml { return this.closeEl(ChainableTags.h3); }
-    h4(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.h4, opts); }
+    h4(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.h4, opts); }
     endH4(): ChainableHtml { return this.closeEl(ChainableTags.h4); }
 
-    ul(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.ul, opts); }
+    ul(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.ul, opts); }
     endUl(): ChainableHtml { return this.closeEl(ChainableTags.ul); }
-    ol(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.ol, opts); }
+    ol(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.ol, opts); }
     endOl(): ChainableHtml { return this.closeEl(ChainableTags.ol); }
-    li(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.li, opts); }
+    li(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.li, opts); }
+    endLi(): ChainableHtml { return this.closeEl(ChainableTags.li); }
 
-    span(opts: BaseChainableOptions): ChainableHtml { return this.baseEl(ChainableTags.span, opts); }
-    endSpan() { return this.closeEl(ChainableTags.span); }
+    span(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.span, opts); }
+    endSpan(): ChainableHtml { return this.closeEl(ChainableTags.span); }
+
+    a(opts:RefOptions = null): ChainableHtml { return this.baseRefEl(ChainableTags.a, opts); }
+    endA(): ChainableHtml { return this.closeEl(ChainableTags.a); }
+
+    button(opts:BaseOptions = null): ChainableHtml { return this.baseEl(ChainableTags.button, opts); }
+    endButton(): ChainableHtml { return this.closeEl(ChainableTags.button); }
+
+    img(opts:ImgOptions = null): ChainableHtml { return this.baseImgEl(ChainableTags.img, opts); }
 }
 export class ChainableClickHtml extends ChainableHtml {
     public constructor(html:string = "") {
         super(html);
     }
-    protected baseEl(tag:string, opts:ClickableOptions): ChainableClickHtml {
+    protected baseEl(tag:string, opts:ClickableOptions = null): ChainableClickHtml {
+        if (opts == null)
+            opts = new ClickableOptions();
         this.html += `<${tag} id='${opts._id}' class='${opts._class}' style='${opts._style}' onclick='${opts._onclick}'>`;
         return this;
     }
-    div(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.div, opts); }
-    p(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.p, opts); }
-    h1(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.h1, opts); }
-    h2(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.h2, opts); }
-    h3(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.h3, opts); }
-    h4(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.h4, opts); }
-    ul(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.ul, opts); }
-    ol(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.ol, opts); }
-    li(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.li, opts); }
-    span(opts: ClickableOptions): ChainableClickHtml { return this.baseEl(ChainableTags.span, opts); }
+    div(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.div, opts); }
+    p(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.p, opts); }
+    h1(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.h1, opts); }
+    h2(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.h2, opts); }
+    h3(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.h3, opts); }
+    h4(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.h4, opts); }
+    ul(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.ul, opts); }
+    ol(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.ol, opts); }
+    li(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.li, opts); }
+    span(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.span, opts); }
+    button(opts:ClickableOptions = null): ChainableClickHtml { return this.baseEl(ChainableTags.button, opts); }
 }
--- spec/ChainableHtmlSpec.ts
+++ spec/ChainableHtmlSpec.ts
@@ -1,22 +1,27 @@
 /// <reference path="../typings/main/ambient/jasmine/index.d.ts" />
 
-import {ChainableHtml, BaseChainableOptions, ChainableClickHtml, ClickableOptions} from "../index";
+import {ChainableHtml, BaseOptions, ChainableClickHtml, ClickableOptions} from "../index";
 
 describe("Functional tests", function() {
     it("creates a basic div", function() {
         var chain = new ChainableHtml();
-        chain = chain.div(new BaseChainableOptions());
-        chain = chain.endDiv();
+        chain = chain.div().endDiv();
         expect(chain.html).toEqual("<div id='' class='' style=''></div>");
     });
     it("escapes single and double quotes in attribute values", function() {
-         var chain = new ChainableHtml();
-        chain = chain.div(new BaseChainableOptions("'oh no\""));
+        var chain = new ChainableHtml();
+        chain = chain.div(new BaseOptions("'oh no\""));
         expect(chain.html).toEqual("<div id='\\'oh no\\\"' class='' style=''>");
     });
     it("chains clickable elements", function() {
         var chain = new ChainableClickHtml();
         chain = chain.div(new ClickableOptions("", "", "", "yay"));
         expect(chain.html).toEqual("<div id='' class='' style='' onclick='yay'>");
+    });
+    it("sets an element's text value", function() {
+        var val = "test value";
+        var chain = new ChainableHtml();
+        chain = chain.div().val(val).endDiv();
+        expect(chain.html).toEqual(`<div id='' class='' style=''>${val}</div>`);
     });
 });
